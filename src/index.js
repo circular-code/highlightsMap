861c2e98dc76ac1ea9bb853d5108d1732f8fb6c0
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2018-08-25 16:09:30 +0100

    merged with create-react-app to introduce service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.unregister();
+//   navigator.serviceWorker
+//     .register("./sw.js", { scope: "../" })
+//     .then(function() {
+//       console.log("Service Worker Registered");
+//     })
+//     .catch(function(err) {
+//       console.log("Service Worker Failed to Register", err);
+//     });
+// }
 
 ReactDOM.render(
   <BrowserRouter>
@@ -10,3 +23,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+registerServiceWorker();
